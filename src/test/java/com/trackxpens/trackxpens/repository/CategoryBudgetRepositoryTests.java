7b5feca9f3e23cbe252764cc6959929bbd0624b2
Gabriel Ortega <gabrielorteganicolas@gmail.com>
2025-03-18 16:19:45 +0100

    Tests for all categories query

--- src/test/java/com/trackxpens/trackxpens/repository/CategoryBudgetRepositoryTests.java
+++ src/test/java/com/trackxpens/trackxpens/repository/CategoryBudgetRepositoryTests.java
@@ -59,8 +59,36 @@ public class CategoryBudgetRepositoryTests {
         assertTrue(foundIds.contains(categoryBudget1.getCategory()));
         assertTrue(foundIds.contains(categoryBudget2.getCategory()));
         assertTrue(foundIds.contains(categoryBudget3.getCategory()));
+    }    
 
+    @Test
+    void correctFindingOneCategory() {
+        CategoryBudget categoryBudget = new CategoryBudget("Test category", 1000);
+        categoryBudgetRepo.save(categoryBudget);
+        idsInserted.add(categoryBudget.getCategory());
+        String foundCategory = categoryBudgetRepo.getAllCategories().get(0);
+        assertEquals(categoryBudget.getCategory(), foundCategory);
+    }
 
+    @Test
+    void correctFindingMultipleCategories() {
+        CategoryBudget categoryBudget1 = new CategoryBudget("Test category 1", 1000);
+        CategoryBudget categoryBudget2 = new CategoryBudget("Test category 2", 2000);
+        CategoryBudget categoryBudget3 = new CategoryBudget("Test category 3", 3000);
+
+        categoryBudgetRepo.save(categoryBudget1);
+        categoryBudgetRepo.save(categoryBudget2);
+        categoryBudgetRepo.save(categoryBudget3);
+
+        idsInserted.add(categoryBudget1.getCategory());
+        idsInserted.add(categoryBudget2.getCategory());
+        idsInserted.add(categoryBudget3.getCategory());
+
+        List<String> foundCategories = categoryBudgetRepo.getAllCategories();
+        assertEquals(3, foundCategories.size());
+        assertTrue(foundCategories.contains(categoryBudget1.getCategory()));
+        assertTrue(foundCategories.contains(categoryBudget2.getCategory()));
+        assertTrue(foundCategories.contains(categoryBudget3.getCategory()));
     }    
 }
 
