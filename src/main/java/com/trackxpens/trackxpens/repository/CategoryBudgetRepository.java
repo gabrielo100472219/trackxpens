65fcbe589b4ba60bcd21f19be9ee020426942902
Gabriel Ortega <gabrielorteganicolas@gmail.com>
2025-03-18 16:09:40 +0100

    Implemented query to get all categories

--- src/main/java/com/trackxpens/trackxpens/repository/CategoryBudgetRepository.java
+++ src/main/java/com/trackxpens/trackxpens/repository/CategoryBudgetRepository.java
@@ -1,8 +1,15 @@
 package com.trackxpens.trackxpens.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.trackxpens.trackxpens.model.CategoryBudget;
 
+@Repository
 public interface CategoryBudgetRepository extends MongoRepository<CategoryBudget, String> {
+    @Query(fields = "{_id: 1}")
+    List<String> getAllCategories();
 }
--- src/main/java/com/trackxpens/trackxpens/repository/ExpenseRepository.java
+++ src/main/java/com/trackxpens/trackxpens/repository/ExpenseRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.trackxpens.trackxpens.model.Expense;
 
+@Repository
 public interface ExpenseRepository extends MongoRepository<Expense, String>{
     @Query("{ '$expr': { '$and': [ { '$eq': [ { '$year': '$date' }, ?0 ] }, { '$eq': [ { '$month': '$date' }, ?1 ] } ] } }")
     List<Expense> findByYearAndMonth(int year, int month);
