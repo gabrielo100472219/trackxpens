028d555cfb053de864efd7a787902da151df14aa
gabrielo <gabrielorteganicolas@gmail.com>
2025-03-18 16:33:36 +0100

    Fixed all categories query

--- src/main/java/com/trackxpens/trackxpens/repository/CategoryBudgetRepository.java
+++ src/main/java/com/trackxpens/trackxpens/repository/CategoryBudgetRepository.java
@@ -2,14 +2,14 @@ package com.trackxpens.trackxpens.repository;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.trackxpens.trackxpens.model.CategoryBudget;
 
 @Repository
 public interface CategoryBudgetRepository extends MongoRepository<CategoryBudget, String> {
-    @Query(fields = "{_id: 1}")
+    @Aggregation(pipeline = { "{ $project: { _id: 1 } }" })
     List<String> getAllCategories();
 }
